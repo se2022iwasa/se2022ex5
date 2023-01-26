8a9f352488a91e33186b7ae49019b3fd8234ac18
iwasaki <r202103307nc@jindai.jp>
2023-01-26 21:42:39 +0900

    3

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(void) {
-  int i = 10;
-  printf("hello world,%d",i);
+  int i;
+  for (i < 10; i++;) {
+    printf("hello world");
+    printf("%d", i);
+  }
   return 0;
 }
